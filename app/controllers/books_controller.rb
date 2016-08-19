b0a9756d0484b739234e45f98589969291069038
Zen <zuzen324@gmail.com>
2016-08-19 17:44:16 +0800

    Add create and show fuction

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,22 @@
 class BooksController < ApplicationController
 	def index
-		@book = Book.all
+		@books = Book.all
 	end
 
 	def new
 		@book=Book.new
+		
+	end
+
+	def create
+
+		@book = Book.new(params.require(:book).permit(:name,:description,:isbn))
+		@book.save
+		redirect_to book_path(@book)
+	end
+
+	def show
+		@book=Book.find(params[:id])
 		
 	end
 end
